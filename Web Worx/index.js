476b3f4bc90b924c6c4bc7e54f3e8c4b26b39215
Prasanna V B <114816129+prasannavb@users.noreply.github.com>
2023-10-18 21:43:49 +0530

    Add files via upload

---
+++ Web Worx/index.js
@@ -0,0 +1,8 @@
+const preload = document.getElementsByClassName("pre-loader");
+const preloadtext = document.getElementsByClassName("pre-load-text");
+const home = document.getElementsByClassName("home");
+const footer = document.getElementsByClassName("footer");
+
+setTimeout(()=>{
+    preload[0].style.display = "none"
+},1200)
